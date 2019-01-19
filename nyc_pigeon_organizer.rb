c731cadaba10bc6b7d2f64a6cb0fe49e1e3502e0
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 03:55:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ pigeon_list = {}
 array = (data[:lives].values).flatten
 count = 0
 until count == array.length
+pigeon_list[array[count]] = {}
 pigeon_list[array[count]][:color] = []
 pigeon_list[array[count]][:gender] = []
 pigeon_list[array[count]][:lives] = []
