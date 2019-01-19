39ee73d31a54c48a14676a93e9792af97c295bd0
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 04:13:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ pigeon_list.each do |names, keys|
   end
   data[:lives].each do |location, name|
     if name.include?(names)
-      pigeon_list[names][:lives].push(location)
+      pigeon_list[names][:lives] << location.to_s
     end
   end
 end
