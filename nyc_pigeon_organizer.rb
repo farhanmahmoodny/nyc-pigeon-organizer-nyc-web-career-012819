7ffd7ba1e58e447d8b90022f0d5bea376e409438
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 03:03:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 def nyc_pigeon_organizer(data)
 pigeon_list = {}
-data[:lives].values.each do |name|
-  pigeon_list[name] = {}
+array = (data[:lives].values).flatten
+count = 0
+until count == array.length
+pigeon_list[array[count]] = {}
 end
 pigeon_list
 end
