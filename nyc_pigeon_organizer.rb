93ce4674c09c4992d179f9390977f12abca36210
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 03:23:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ pigeon_list[array[count]] = {}
 count += 1
 end
 data[:color].each do |color, name|
-  if name.include?("Theo")
+  if name.values.include?("Theo")
     pigeon_list["Theo"] << color
   end
 end
