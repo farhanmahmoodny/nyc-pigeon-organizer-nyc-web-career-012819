52560acdb67204adbe83e81751e1151e8d356978
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 03:39:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,13 +6,6 @@ until count == array.length
 pigeon_list[array[count]] = {}
 count += 1
 end
-p_stats = data.keys
-pigeon_list.each do |name|
- count = 0
- until count = data.length
- name[data[count]] = {}
- count += 1
- end
-end  
+
 pigeon_list
 end
